2776f168c4ec32d927840d22448af249f178354a
Lázaro Henrique Silva <lazhcsilva@gmail.com>
2023-04-26 22:55:36 -0300

    Criando backend do projeto

---
+++ backend/src/main/java/br/com/agendamentolab/AgendamentolabApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendamentolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentolabApplication.class, args);
+	}
+
+}
